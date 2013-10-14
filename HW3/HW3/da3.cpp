eed50d422c4ba1855f7a7797ceba6bceb3bd1113
arsh25 <achauhan@alaska.edu>
2013-10-14 13:11:33 -0800

    Optimized collatz(int) return statement. After compiling i noticed it didn't matter since the compiler was anyways automatically optimizing it.

--- HW3/HW3/da3.cpp
+++ HW3/HW3/da3.cpp
@@ -14,13 +14,11 @@ int collatz(int n)
 	
 		if( n % 2 == 0)
 	{
-		n = n/2;
-		return 1+collatz(n);
+		return 1+collatz(n/2);
 	}
 	else 
 	{
-		n = (3*n)+1;
-		return 1+collatz(n);
+		return 1+collatz((3*n)+1);
 	}
 	}
 	return 0;
