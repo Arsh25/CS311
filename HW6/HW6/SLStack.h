763e24a50e285cb719a4245f21fc24decc3080c8
arsh25 <achauhan@alaska.edu>
2013-12-04 19:04:38 -0900

    Initial commit for HW6. its doesn't even compile BITCHES !!!!

---
+++ HW6/HW6/SLStack.h
@@ -0,0 +1,80 @@
+#ifndef SLStack_H
+#define SLStack_H
+
+#include "slist.h"
+
+template<class T>
+class SLStack
+{
+
+public:
+SLStack();						// default ctor //
+SLStack(const SLStack &orig); 	// copy constructor //
+~SLStack();						// destructor //
+
+bool empty();					// returns a bool indicating if Stack is empty //
+SLStack & top();				// returns a reference to item on Top of Stack //
+void push(value_type);			// moves inputted value to stack //
+void pop();						// gives back the top item on stack //
+
+private:
+SList * top; 					// pointer to stack
+};
+
+template<class T>
+SLStack<T>::SLStack()
+{
+top = 0;
+}
+
+template<class T>
+SLStack<T>::SLStack(const SLStack &orig)
+{
+}
+
+template<class T>
+SLStack<T>::~SLStack()
+{
+}
+
+template<class T>
+bool SLStack<T>::empty()
+{
+if (top == 0) { return true; }		// nothing on the stack? return true //
+else { return false; }				// something is there? return false //
+}
+
+// pre-conditions: stack exists //
+// post-conditions: returns reference to what ever was at the top of the stack //
+template<class T>
+SLSTack & SLStack<T>::top()	
+{
+
+}
+
+// pre-conditions: none
+// post-conditions input data is moved to top of stack //
+template<class T>
+void SLStack<T>::push(value_type a)
+{
+/*
+SList * p = new SList(a);			// something I found on the internet //
+p -> next=top;						// may not even be good //
+top = p;
+*/
+}
+
+// pre-conditions:
+// post-conditions:
+template<class T>
+void SLStack<T>::pop()		// should we be calling SLStack::top() here? //
+{							 
+/*
+SList * p = top;
+top = top->next;				// I'll admit I don't know what's going on here //
+value_type x=p->_data;			// some stuff I found online //
+delete p;
+*/
+}
+
+#endif
---
+++ HW6/HW6/slist.h
@@ -0,0 +1,95 @@
+
+#ifndef SLIST_H_
+#define SLIST_H_
+
+#include<cstdlib> //for std::size_t
+#include<algorithm>
+
+
+template <class T>
+
+class SList
+{
+public:
+	typedef T value_type;
+	typedef std::size_t size_type;
+	
+	SList();							//default ctor//
+	SList(const SList &orig);			//copy ctor//
+	~SList();							//Dctor//
+
+	SList & operator=(SList rhs);		//copy assignment//
+
+	size_type size() const;				//size function, returns size of list//
+	template <typename InputIterator>	//input iterator, used in the read function//
+	void read(InputIterator first, InputIterator Last); //read function, reads from list between first and last//
+	template <typename OutputIterator>	//output interator - for write starting at iterator's position//
+	void write(OutputIterator dest) const; //uses OutputIterator as a starting point//
+	void reverse();						// reverses order of Linked list//
+private:
+
+	value_type _data; // Data for this node 
+	//size_t _size;
+	SList * _next; // Ptr to next node, NULL if empty or last item 
+};
+
+template <class T>
+SList<T>::SList():_next(NULL)			// default constructor //
+{
+}
+
+template <class T>
+SList<T>::SList( const SList &orig)		//copy constructor//
+{
+	_data = orig._data;
+	_next = orig->_next;
+
+}
+
+template <class T>
+SList<T>& SList<T>::operator=(SList<T> rhs)	// copy assignment //
+{
+	std::swap(rhs->_next,_next);
+	std::swap(rhs._data,_data);
+
+	return *this;
+}
+
+template <class T>
+typename SList<T>::size_type SList<T>::size() const 
+{
+	size_type counter = 0;
+	while ( _next != NULL)// infinite loop currently, I THINK 
+	{
+		++counter;
+	}
+}
+
+template <class T>
+template <typename InputIterator>
+
+void SList<T>::read( InputIterator first, InputIterator last)
+{
+	delete _data;
+	_next = NULL:
+	while(first != last)
+	{
+		value_type val = *first++;
+		_data = val;
+		_next = first + 1;
+	}
+
+}
+
+template<class T>
+void SList<T>::reverse()
+{
+	/*for(int i = 0; i < (Slist.size()/2); i++)
+	{
+		std::iter_swap(  
+	*/
+}
+
+
+#endif 
+
