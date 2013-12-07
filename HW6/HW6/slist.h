ff38409a606550b0c4fbdd5bac63edac1fa81a51
sjgeorge2 <sjgeorge34@yahoo.com>
2013-12-06 19:40:24 -0800

    Update slist.h

--- HW6/HW6/slist.h
+++ HW6/HW6/slist.h
@@ -1,4 +1,3 @@
-
 #ifndef SLIST_H_
 #define SLIST_H_
 
@@ -91,23 +90,39 @@ void SList<T>::read( InputIterator first, InputIterator last)
 template<class T>
 void SList<T>::reverse()
 {
-	/*for(int i = 0; i < (Slist.size()/2); i++)
-	{
-		std::iter_swap(  
-	*/
-}
-template<typename OutputIterator>
-template<class T>
-void SList<T>::write( OutputIterator dest)const
-{
+	InputIterator fromFirst;
+	fromFirst = _next;				// fromFirst now points to the first item
 
-	for (auto ii=_next;_next!=NULL;_next++)
+	InputIterator fromLast;
+	for (int i = 0; i < Slist.size()-1; i++)	// goes through the loop until it reaches the last item //
+	{	
+							// points _next to the next item //
+		if (_next++ == NULL)			// if the next item points to NULL we should be at the end //
+		{
+			fromLast = _next;		// fromLast now points to the last item //
+		}
+		++_next;				// points _next to the next item //
+	}
+	
+	int alternate = 1;				// used to alternate increments and decrements //
+	while (fromFirst != fromLast)			// while not equal, will leave out the middle item //
 	{
-		value_type val;
-		val = _data;
-		*dest++ = val;
-		//++_next;
+		if (alternate == 1)
+		{
+			std::swap_Iter(fromFirst, fromLast);
+			fromLast++;				// because last now points at the start we increment //
+			fromFirst--;				// because first now points to the end we decrement //
+			alternate = 2;				// alternate //
+		}
+		if (alternate == 2)
+		{
+			std::swap_Iter(fromFirst, fromLast);
+			fromLast--;				// last now points to the end, we decrement //
+			fromFirst++;				// first now points to the start, we increment //
+			alternate = 1;				// alternate //
+		}
 	}
 }
+
 #endif 
 
